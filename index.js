6d442edcfe7cce737b4a7b59f4e26d0c572e4378
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-21 00:10:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  var object=Object.assign({},object)
+  object.key=[value]
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
